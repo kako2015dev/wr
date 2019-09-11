cb7873bd016356ef75d6cd736112bdbd9f997d8d
kako2015dev <kako2015.4@gmail.com>
2019-09-11 10:56:16 +0400

    funct implementation

--- wr/Program.cs
+++ wr/Program.cs
@@ -14,33 +14,29 @@ namespace wr
             string testString = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
             // ძირითადი "ენჟინის" შექმნა
-            lib.WRLib wr = new lib.WRLib();
+            lib.WRLib wr1 = new lib.WRLib();
 
             // საწყისი მონაცემების დაფიქსირება კლასისი ეგზემპლიარის wr ერთერთ თვისებაში inStr ში
-            wr.inStr = testString;
-
-            // წინადადების დაშლა სიტყვების სიად
-            List<string> lstW = wr.Str2List(testString);
+            wr1.inStr = testString;
 
             // ერთი ქვესტრიქონის სიგრძე
-            wr.insideStringWidth = 60;
+            wr1.insideStringWidth = 60;
+
+            // წინადადების დაშლა სიტყვების სიად
+            List<string> lstW = wr1.Str2List(testString);
 
             // სიტყვების სიის დაშლა "სიტყვების ქვესიებად" თითველი ქვესიის სიგრძე არ უნდა აღემატებოდეს ქვესტრიქონის სიგრძეს (პრობელების ჩათვლით)
-            List<List<string>> ll = wr.lst2subLists(lstW);
+            List<List<string>> ll = wr1.lst2subLists(lstW);
 
+            // სიტყვების გადაბმა ქვესტრინგებად და გათანაბრება
+            List<string> subStrings = wr1.subWords2sibStrings(ll);
 
-            List<string> subStrings = new List<string>();
-            foreach (var subStrList in ll)
-            {
-                string s0 = wr.list2str(subStrList);
-                subStrings.Add(s0);
-            }
+            // ვარსკვლავების დამატება
+            List<string> subStringsWithStars = wr1.addStars(subStrings);
 
-            List<string> subStringsWithStars = wr.addStars(subStrings);
-
-            wr.drawFirstLastRow();
-            wr.drawMidleRows(subStringsWithStars);
-            wr.drawFirstLastRow();
+            wr1.drawFirstLastRow();
+            wr1.drawMidleRows(subStringsWithStars);
+            wr1.drawFirstLastRow();
 
 
             Console.ReadKey();
--- wr/lib/WRLib.cs
+++ wr/lib/WRLib.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace wr.lib
 {
-    public class WRLib
+    class WRLib
     {
         public string inStr { get; set; }
+        public string star { get; set; }
+        public int zoma { get; set; }
+        public string separ { get; set; }
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public WRLib()
+        {
+            inStr = @"rame sitkvebi";
+            star = "*";
+            zoma = 60;
+            separ = " ";
+        }
         public int insideStringWidth { get; internal set; }
 
         public List< string > Str2List(string s)
@@ -20,27 +33,98 @@ namespace wr.lib
 
         internal List<List<string>> lst2subLists(List<string> lstW)
         {
-            throw new NotImplementedException();
+            List<List<string>> res = new List<List<string>>();
+            List<string> subLineWords = new List<string>();
+            string subLine = "";
+            foreach (var w in lstW)
+            {
+                
+                if (subLine.Length + w.Length + 1 <= this.zoma)
+                {
+                    subLine += this.separ + w;
+                    subLineWords.Add(w);
+                }
+                else
+                {
+                    res.Add(subLineWords);
+
+                    subLineWords = new List<string>();
+                    subLine = "";
+                    subLine += this.separ + w;
+                    subLineWords.Add(w);
+
+                }
+            }
+            res.Add(subLineWords);
+            return res;
+            //throw new NotImplementedException();
         }
 
         internal string list2str(List<string> subStrList)
         {
-            throw new NotImplementedException();
+            string res = "";
+            foreach (var w in subStrList)
+            {
+                res += " " + w;
+            }
+            return res;
+            //throw new NotImplementedException();
         }
+
+        internal string gatanabreba(string word)
+        {
+            string res = "";
+            res = word + this.rep(this.separ, this.zoma - word.Length);
+
+            return res;
+            //throw new NotImplementedException();
+        }
+
+        internal List<string> subWords2sibStrings(List<List<string>> ll)
+        {
+            List<string> subStrings = new List<string>();
+            foreach (var subStrList in ll)
+            {
+                string s0 = this.list2str(subStrList);
+                subStrings.Add(this.gatanabreba(s0));
+            }
+            return subStrings;
+        }
+
 
         internal List<string> addStars(List<string> subStrings)
         {
-            throw new NotImplementedException();
+            List<string> subStringsWithStars = new List<string>();
+            foreach (var subStr in subStrings)
+            {
+                string s;
+                s = this.star + subStr + this.star;
+                subStringsWithStars.Add(s);
+            }
+            return subStringsWithStars;
+
+            //throw new NotImplementedException();
         }
 
         internal void drawFirstLastRow()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(this.FirstLastLine());
         }
-
+        private string FirstLastLine()
+        {
+            return this.rep(this.star, this.zoma);
+        }
+        private string rep(string oneChar, int zoma)
+        {
+            return new string(oneChar[0], zoma);
+        }
         internal void drawMidleRows(List<string> subStringsWithStars)
         {
-            throw new NotImplementedException();
+            foreach (var midLine in subStringsWithStars)
+            {
+                Console.WriteLine(midLine);
+            }
+            //throw new NotImplementedException();
         }
     }
 }
